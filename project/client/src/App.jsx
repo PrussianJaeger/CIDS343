eb7aa6f82d17b56cd00e547f2301086cd714fc3b
Westini <westinimpola1@gmail.com>
2025-03-27 13:36:29 -0500

    update nav bar

--- project/client/src/App.jsx
+++ project/client/src/App.jsx
@@ -1,8 +1,20 @@
 import ListGroup from './Components/ListGroup';
+import Header from "./Header";
+import DbDisplay from "./DbDisplay";
+import BackendDisplay from "./BackendDisplay";
+import Footer from "./Footer";
+import Side from './Side';
 
 function App() {
 
-  return <div><ListGroup /></div>;
+  return <>
+    <ListGroup />
+    <DbDisplay></DbDisplay>
+    <BackendDisplay></BackendDisplay>
+    <Footer></Footer>
+
+  </>
+
 }
 
 export default App;
