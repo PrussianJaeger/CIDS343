079c35ed300895c1cb02d0dfb015a61f03ef4a15
PrussianJaeger <prussianjaeger@proton.me>
2025-05-01 14:41:28 -0500

    dark mode

--- client/src/Footer.jsx
+++ client/src/Footer.jsx
@@ -3,6 +3,7 @@ function Footer() {
     <>
       <div className="footer">
         <p>CIDS 343 Project - Spring 2025</p>
+        <p id="authors">Westin Impola, Luke Iovino, Sam Johnson, Will Weiskopf</p>
       </div>
     </>
   );
