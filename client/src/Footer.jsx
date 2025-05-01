8ccf8235a15b23962a230658c7d1909c96ce8aff
PrussianJaeger <prussianjaeger@proton.me>
2025-05-01 15:17:05 -0500

    css changes BIG

--- client/src/Footer.jsx
+++ client/src/Footer.jsx
@@ -2,8 +2,8 @@ function Footer() {
   return (
     <>
       <div className="footer">
-        <p>CIDS 343 Project - Spring 2025</p>
-        <p id="authors">Westin Impola, Luke Iovino, Sam Johnson, Will Weiskopf</p>
+        <div>CIDS 343 Project - Spring 2025</div>
+        <div id="authors">Joey Fellrath, Westin Impola, Luke Iovino, Sam Johnson, Will Weiskopf</div>
       </div>
     </>
   );
